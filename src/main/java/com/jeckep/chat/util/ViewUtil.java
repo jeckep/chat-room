e5b009c5f55ac4d23e59b4cdf08dada744999170
Yevgeniy Poluektov <evgeniy.v.poluektov@gmail.com>
2016-09-09 21:54:48 +0300

    use one velocity engine, because it is threadsafe performance increased > 10 times tested by wrk

--- src/main/java/com/jeckep/chat/util/ViewUtil.java
+++ src/main/java/com/jeckep/chat/util/ViewUtil.java
@@ -4,6 +4,7 @@ import org.apache.velocity.Template;
 import org.apache.velocity.app.*;
 import org.apache.velocity.exception.ParseErrorException;
 import org.apache.velocity.exception.ResourceNotFoundException;
+import org.apache.velocity.runtime.RuntimeConstants;
 import org.eclipse.jetty.http.HttpStatus;
 import spark.ModelAndView;
 import spark.Request;
@@ -37,16 +38,22 @@ public class ViewUtil {
         return render(request, new HashMap<>(), Path.Template.NOT_FOUND);
     };
 
+    private static VelocityTemplateEngine engine;
     private static VelocityTemplateEngine strictVelocityEngine() {
-        VelocityEngine configuredEngine = new VelocityEngine(){
-            @Override
-            public Template getTemplate(String name) throws ResourceNotFoundException, ParseErrorException {
-                return super.getTemplate(name, "UTF-8");
-            }
-        };
-        configuredEngine.setProperty("runtime.references.strict", true);
-        configuredEngine.setProperty("resource.loader", "class");
-        configuredEngine.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
-        return new VelocityTemplateEngine(configuredEngine);
+        if(engine == null) {
+            VelocityEngine configuredEngine = new VelocityEngine() {
+                @Override
+                public Template getTemplate(String name) throws ResourceNotFoundException, ParseErrorException {
+                    return super.getTemplate(name, "UTF-8");
+                }
+            };
+            configuredEngine.setProperty("runtime.references.strict", true);
+            configuredEngine.setProperty(RuntimeConstants.PARSER_POOL_SIZE, 100);
+            configuredEngine.setProperty(RuntimeConstants.VM_LIBRARY_AUTORELOAD, false);
+            configuredEngine.setProperty("resource.loader", "class");
+            configuredEngine.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+            engine = new VelocityTemplateEngine(configuredEngine);
+        }
+        return engine;
     }
 }
