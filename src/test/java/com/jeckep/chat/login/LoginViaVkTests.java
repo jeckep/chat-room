317dbd8e61a07dc04d277a7dc65f708bc3608abe
Yevgeniy Poluektov <evgeniy.v.poluektov@gmail.com>
2016-09-14 17:46:37 +0300

    add redirect to vk test

---
+++ src/test/java/com/jeckep/chat/login/LoginViaVkTests.java
@@ -0,0 +1,39 @@
+package com.jeckep.chat.login;
+
+import com.jeckep.chat.util.Path;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DirtiesContext
+public class LoginViaVkTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void loginRedirectsToVK() throws Exception {
+		ResponseEntity<Void> entity = this.restTemplate.getForEntity(Path.Web.LOGIN_VK,
+				Void.class);
+		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.FOUND);
+		assertThat(entity.getHeaders().getLocation().toString())
+				.startsWith("https://oauth.vk.com/authorize");
+	}
+
+}
