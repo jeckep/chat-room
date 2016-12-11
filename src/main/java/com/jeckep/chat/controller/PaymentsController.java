159b1fbd6bf089498aa642ea01ae4aae29431ebf
Yevgeniy Poluektov <evgeniy.v.poluektov@gmail.com>
2016-12-11 23:18:03 +0300

    customize yandex payment form

--- src/main/java/com/jeckep/chat/controller/PaymentsController.java
+++ src/main/java/com/jeckep/chat/controller/PaymentsController.java
@@ -6,13 +6,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Map;
 
 @Controller
 public class PaymentsController {
+    private static final String FORM_URL = "https://money.yandex.ru/quickpay/shop-widget?account=410013589656448&quickpay=shop&payment-type-choice=on&mobile-payment-type-choice=on&writer=seller&button-text=01&comment=on&successURL=jeckep.online";
+
     @GetMapping(Path.Web.PAYMENTS)
-    public String index(Map<String, Object> model, HttpServletRequest request) {
+    public String index(Map<String, Object> model, HttpServletRequest request) throws UnsupportedEncodingException {
+        final String formUrl = FORM_URL
+                 + "&sum=321"
+                + "&default-sum=123"
+                + "&label=12345678"
+                + "&targets=" + URLEncoder.encode("На шлюх и наркотики Женьку", "UTF8")
+                + "&hint=" + URLEncoder.encode("Впишите сюда что-нибудь", "UTF8")
+                ;
         model.put("nav_active", "payments");
+        model.put("formUrl", formUrl);
         ViewUtil.putLayoutVars(request, model);
         return Path.Template.PAYMENTS;
     }
